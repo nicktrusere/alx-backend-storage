9917ee51ffc458310ce5c8ebc720e8c8c38ca90e
Nicktrusere <Nicktrusere@gmail.com>
2023-10-18 16:52:22 +0000

    an expiring web cache and tracker

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,24 +1,51 @@
 #!/usr/bin/env python3
-'''A module with tools for request caching and tracking.
-'''
+"""web module
+"""
+from functools import wraps
+from typing import Callable
 import redis
 import requests
-from datetime import timedelta
+
+_redis = redis.Redis()
+_redis.flushdb()
 
 
+def count_requests(method: Callable) -> Callable:
+    """count_requests function
+
+    Args:
+        method (Callable): method
+
+    Returns:
+        Callable: wrapper
+    """
+    @wraps(method)
+    def wrapper(*args, **kwargs):
+        """wrapper function
+
+        Returns:
+            [type]: wrapper
+        """
+        url = args[0]
+        cached = _redis.get(f"cached:{url}")
+        if cached:
+            return cached.decode("utf-8")
+        response = method(*args, **kwargs)
+        _redis.incr(f"count:{url}")
+        _redis.setex(f"cached:{url}", 10, response)
+        return response
+    return wrapper
+
+
+@count_requests
 def get_page(url: str) -> str:
-    '''Returns the content of a URL after caching the request's response,
-    and tracking the request.
-    '''
-    if url is None or len(url.strip()) == 0:
-        return ''
-    redis_store = redis.Redis()
-    res_key = 'result:{}'.format(url)
-    req_key = 'count:{}'.format(url)
-    result = redis_store.get(res_key)
-    if result is not None:
-        redis_store.incr(req_key)
-        return result
-    result = requests.get(url).content.decode('utf-8')
-    redis_store.setex(res_key, timedelta(seconds=10), result)
-    return result
+    """get_page function
+
+    Args:
+        url (str): url
+
+    Returns:
+        str: response
+    """
+    response = requests.get(url, timeout=10)
+    return response.text
